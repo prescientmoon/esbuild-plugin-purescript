fe9ff736beb39cf8df1267d68b0cbcad6bb916cb
Matei Adriel <rafaeladriel11@gmail.com>
2021-05-24 22:22:55 +0300

    build: show build errors

--- build.js
+++ build.js
@@ -1,12 +1,10 @@
 const esbuild = require("esbuild");
 
-esbuild
-  .build({
-    entryPoints: ["src/index.js"],
-    bundle: true,
-    outdir: ".",
-    plugins: [],
-    platform: "node",
-    minify: true,
-  })
-  .catch((_e) => process.exit(1));
+esbuild.build({
+  entryPoints: ["src/index.js"],
+  bundle: true,
+  outdir: ".",
+  plugins: [],
+  platform: "node",
+  minify: true,
+});
