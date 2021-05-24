2d03ed75e5b67abf2bc18a5c6cf1c16b7f5d9c40
Matei Adriel <rafaeladriel11@gmail.com>
2021-05-24 22:25:56 +0300

    build: perhaps it actually builds it this time?

--- build.js
+++ build.js
@@ -1,10 +1,15 @@
 const esbuild = require("esbuild");
 
-esbuild.build({
-  entryPoints: ["src/index.js"],
-  bundle: true,
-  outdir: ".",
-  plugins: [],
-  platform: "node",
-  minify: true,
-});
+esbuild
+  .build({
+    entryPoints: ["src/index.js"],
+    bundle: true,
+    outdir: ".",
+    plugins: [],
+    platform: "node",
+    minify: true,
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
